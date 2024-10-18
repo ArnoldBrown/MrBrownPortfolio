6abfd6cb2d35bf27cf9da045ddd590be87f23c18
ArnoldBrown <arnoldbrown033@gmail.com>
2024-10-18 09:46:48 +0530

    personal_data

---
+++ src/data/personal-data.js
@@ -0,0 +1,15 @@
+export const personalData = {
+    name: "ARNOLD BROWN",
+    profile: '/profile.png',
+    designation: "Sr. Software Developer",
+    description: "My name is ARNOLD BROWN. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of mobile application development and love to make the mobile more open to the world. My core skill is based on JavaScript, Java, Kotlin. I am available for any kind of job opportunity that suits my skills and interests.",
+    email: 'arnoldbrown033@gmail.com',
+    phone: '+919629604689',
+    address: '189, Royal Nager, Nagercoil - 629003',
+    github: 'https://github.com/arnoldbrown',
+    facebook: 'https://www.facebook.com/arnoldbrown/',
+    linkedIn: 'https://www.linkedin.com/in/arnoldbrown33/',
+    twitter: 'https://twitter.com/arnoldbrown',
+    stackOverflow: 'https://stackoverflow.com/users/5049244/arnold-brown',
+    resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
+  }
